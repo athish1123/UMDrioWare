480f082342315b8ec987dccf40fe65444d7bd63e
Aria Williams <awill9579@gmail.com>
2026-02-25 14:19:46 -0600

    Are you rushing or are you dragging

--- Frameworks(YourStuff)/EldritchGame/BigGuy/monsta.gd
+++ Frameworks(YourStuff)/EldritchGame/BigGuy/monsta.gd
@@ -8,11 +8,21 @@ var adaptive_music : EldritchAdaptiveMusic
 
 func _ready() -> void:
 	adaptive_music = get_tree().get_first_node_in_group("EldritchAdaptiveMusic")
-	eating_zone.planet_suck.connect(move_planet_to_marker)
+	eating_zone.planet_suck.connect(suck_in_planet)
+
+func suck_in_planet(planet : Planet):
+	await adaptive_music.current_beat
+	if adaptive_music.bar_index % 4 == 1:
+		print("Good hit!")
+	elif adaptive_music.bar_index % 4 == 2:
+		print("Dragging!")
+	else:
+		print("Rushing!")
+	
+	clap.play()
+	move_planet_to_marker(planet)
 
 func move_planet_to_marker(planet : Planet) -> void:
-	await adaptive_music.current_beat
-	clap.play()
 	var tween : Tween = create_tween()
 	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(planet, 'global_position', marker_2d.global_position, 0.2)
